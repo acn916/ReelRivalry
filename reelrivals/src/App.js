6fbb325d6821616a6a77942328debf307f4c36f5
acn916 <austin.n916@gmail.com>
2024-10-23 11:58:13 -0700

    Added private routes in app.js and created authcontext for user data to be used throughout project

--- reelrivals/src/App.js
+++ reelrivals/src/App.js
@@ -5,45 +5,62 @@ import Navbar from "./components/navbar";
 import Signup from "./scenes/signup";
 import Login from "./scenes/login";
 import Dashboard from "./scenes/dashboard";
+import CreateTournament from "./scenes/createTournament";
+import { useAuth } from "./AuthContext";
+import { Navigate } from "react-router-dom";
 
 
 import { createTheme, themeProvider} from "@mui/material/styles";
 import { ThemeProvider } from "@emotion/react";
 import CssBaseline from '@mui/material/CssBaseline';
 
+import { AuthProvider } from "./AuthContext";
 
+  const theme = createTheme({
+      palette: {
+          primary:{
+            main: '#282525',
+          },
+          secondary:{
+            main: '#D9D9D9',
+          },
+          third:{
+            main: '#919191'
+          }
+      },
+  });
 
-
-const theme = createTheme({
-  palette: {
-    primary:{
-      main: '#282525',
-    },
-    secondary:{
-      main: '#D9D9D9',
-    },
-  },
-});
+  function PrivateRoute({ children }) {
+      const { currentUser } = useAuth();
+      return currentUser ? children : <Navigate to="/login" />;
+  }
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        
+    return (
+      <AuthProvider>
+          <ThemeProvider theme={theme}>
+              <CssBaseline/>
+              <Navbar/>
+              <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/signup" element={<Signup/>}/>
 
+
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
+                <Route path="/create-tournaments" element={<PrivateRoute><CreateTournament/></PrivateRoute>}/>
+                
+
+              
+              </Routes>
+
+          </ThemeProvider>
+
+      </AuthProvider>
+          
       
-      </Routes>
-
-    </ThemeProvider>
-    
-    
-  );
+      
+    );
 }
 
 export default App;
